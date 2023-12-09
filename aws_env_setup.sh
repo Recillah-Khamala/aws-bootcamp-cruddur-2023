586fdcc170fb534ef910ede5020ff0fe570171c4
recillah khamala <recillahk@gmail.com>
2023-12-09 05:11:10 +0000

    add a script

---
+++ aws_env_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Source the file containing environment variables
+source /workspace/aws-bootcamp-cruddur-2023/env_variables
+
+# Optionally, configure AWS CLI default output format
+aws configure set default.output json
